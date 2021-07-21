f280d8e6d940e7d67611050f7d948f740276152d
Rahul Choudhary <rahul.30213021@gmail.com>
2021-07-21 23:27:01 +0530

    made changes to front end

--- front-end/script/form.js
+++ front-end/script/form.js
@@ -1,20 +1,19 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDKcPIMTtBcCr5CH2e5AKFmFya8z8vP81g",
-    authDomain: "credibility-analyzer-3c82f.firebaseapp.com",
-    projectId: "credibility-analyzer-3c82f",
-    storageBucket: "credibility-analyzer-3c82f.appspot.com",
-    messagingSenderId: "373902736528",
-    appId: "1:373902736528:web:703cc3797a9fc88b51ec15",
-   // measurementId: "G-YPVY8Q4DLQ"
-  };
+  apiKey: "AIzaSyDKcPIMTtBcCr5CH2e5AKFmFya8z8vP81g",
+  authDomain: "credibility-analyzer-3c82f.firebaseapp.com",
+  projectId: "credibility-analyzer-3c82f",
+  storageBucket: "credibility-analyzer-3c82f.appspot.com",
+  messagingSenderId: "373902736528",
+  appId: "1:373902736528:web:703cc3797a9fc88b51ec15",
+  // measurementId: "G-YPVY8Q4DLQ"
+};
 
 firebase.initializeApp(firebaseConfig);
 
 //firebase.analytics();
 const auth = firebase.auth();
 
-function signUp()
-{
+function signUp() {
   var email = document.getElementById("email");
   var password = document.getElementById("password");
   const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
@@ -25,32 +24,29 @@ function signUp()
   ).catch((error) => {
     console.log(error["code"]);
     alert(error);
-  });   
+  });
   // console.log(promise);
   // promise.catch(e => alert(e.message));
 
   // alert("Signed Up");
-}  
-
-function signIn()
-{
-var email = document.getElementById("email");
-var password = document.getElementById("password");
-var res;
-// const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-firebase.auth().signInWithEmailAndPassword(email.value, password.value).then(res => {
-  console.log(res);
-  window.location.href = "./pages/submission.html";
-}
-).catch((error) => {
-  console.log(error["code"]);
-  alert(error);
-});
 }
 
-function signOut()
-{
-  auth.signOut();
+function signIn() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  var res;
+  // const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+  firebase.auth().signInWithEmailAndPassword(email.value, password.value).then(res => {
+    console.log(res);
+    window.location.href = "./pages/submission.html";
+  }
+  ).catch((error) => {
+    console.log(error["code"]);
+    alert(error);
+  });
+}
+
+function signOut() {
   window.location.href = "../index.html";
   console.log("signed out");
   alert("Signed out");
---
+++ front-end/script/form2.js
@@ -0,0 +1,124 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+	apiKey: "AIzaSyB1S6V1jdDMCK8RLihSlJ0kR1uMVeDJZlM",
+	authDomain: "credibility-analyser.firebaseapp.com",
+	projectId: "credibility-analyser",
+	storageBucket: "credibility-analyser.appspot.com",
+	messagingSenderId: "937128837506",
+	appId: "1:937128837506:web:11e508fda0baf94c570f9b",
+	measurementId: "G-J6E68069FX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const database = firebase.database();
+
+var theUserId;
+
+async function signUp() {
+	//Jai bhole nath
+	//get the email id and password
+	var email = document.getElementById("email");
+	var password = document.getElementById("password");
+	if (email.value == "" || password.value == "") {
+		alert('Please input both ID and password');
+		return;
+	}
+	console.log(email.value + '-' + password.value);
+	var duplicateFound = false;
+	//Look for the email in the records, if it exist then tell it exists
+	var token = await database.ref('/users/');
+	await token.once('value', (snapshot) => {
+		const data = snapshot.val();
+		for (var i in data) {
+			// console.log(data[i]["email"]);
+			if (data[i]["email"] == email.value) {
+				console.log('Duplicate found');
+				alert('The id already exists, please login\n');
+				duplicateFound = true;
+				return;
+			}
+		}
+	});
+	if (duplicateFound == false) {
+		token.push().set({
+			email: email.value,
+			password: password.value,
+		});
+		alert('Account created');
+	}
+}
+
+async function signIn() {
+	var email = document.getElementById("email");
+	var password = document.getElementById("password");
+	if (email.value == "" || password.value == "") {
+		alert('Please input both ID and password');
+		return;
+	}
+	var idFound = false;
+	var token = await database.ref('/users/');
+	await token.once('value', (snapshot) => {
+		const data = snapshot.val();
+		for (var i in data) {
+			if (data[i]["email"] == email.value && data[i]["password"] == password.value) {
+				theUserId = i;
+				sessionStorage.setItem("userKey", theUserId);
+				console.log('ID is' + i);
+				idFound = true;
+				window.location.href = "./pages/selectSuspect.html";
+				// alert('Logged in!');
+			}
+		}
+	});
+	if (idFound == false) {
+		alert('Please enter correct id and password');
+		return;
+	}
+}
+
+function signOut() {
+	window.location.href = "../index.html";
+	console.log("signed out");
+	alert("Signed out");
+}
+
+function showInputForm() {
+	document.getElementById("showInputForm").style.display = "block";
+}
+
+function showSelectSuspect() {
+
+}
+
+async function storeSuspectData() {
+	var name = document.getElementById("name").value;
+	var height = document.getElementById("height").value;
+	var weight = document.getElementById("weight").value;
+	var idnumber = document.getElementById("idnumber").value;
+	var dob = document.getElementById("dob").value;
+	var crime = document.getElementById("crime").value;
+	var hypo = document.getElementById("hypo").value;
+	var gender;
+
+	if (document.getElementById("male").checked) {
+		gender = 'male';
+	}
+	else if (document.getElementById("female").checked) {
+		gender = 'female';
+	}
+	var token = await database.ref('/users/' + sessionStorage.getItem("userKey") + '/suspectList/');
+	console.log(sessionStorage.getItem("userKey") + ' ' + name + ' ' + height + ' ' + weight + ' ' + idnumber + ' ' + dob + ' ' + gender + ' ' + crime + ' ' + hypo);
+	await token.push().set({
+		name: name,
+		height: height,
+		weight: weight,
+		idnumber: idnumber,
+		dob: dob,
+		gender: gender,
+		crime: crime,
+		hypo: hypo,
+	});
+	alert('Record added successfully');
+}
